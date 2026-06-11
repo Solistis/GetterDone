fa5a94343225fd531b5d980547f17623494451d0
Solistis <Solistis01@gmail.com>
2026-06-10 22:15:04 -0500

    Added a list detail update and delete routes

--- backend/src/routes/listRoutes.js
+++ backend/src/routes/listRoutes.js
@@ -84,4 +84,119 @@ router.get("/:id/items", async (req, res) => {
     }
 });
 
+// get or retrieve a list or category by ID
+router.get("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await pool.query(
+            `
+            SELECT * 
+            FROM list
+            WHERE list_id = $1
+            `,
+            [id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "List not found",
+            });
+        }
+
+        res.json({
+            success: true,
+            list: result.rows[0],
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+        success: false,
+        error: error.message,
+        });
+    }
+});
+
+// Update category list
+router.put("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, description, color, icon } = req.body || {};
+
+        const result = await pool.query(
+        `
+        UPDATE list
+        SET
+            name = COALESCE($1, name),
+            description = COALESCE($2, description),
+            color = COALESCE($3, color),
+            icon = COALESCE($4, icon)
+        WHERE list_id = $5
+        RETURNING *
+        `,
+        [name, description, color, icon, id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "List not found",
+            })
+        }
+
+        res.json({
+            success: true,
+            list: result.rows[0],
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+        success: false,
+        error: error.message,
+        });
+    }
+});
+
+// DELETE list category
+router.delete("/:id", async (req, res) => {
+    try{
+        const { id } = req.params;
+
+        const result = await pool.query(
+            `
+            DELETE FROM list
+            WHERE list_id = $1
+            RETURNING *
+            `,
+            [id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                succuss: false,
+                message: "List not found",
+            });
+        }
+
+        res.json({
+            success: true,
+            message: "List deleted",
+            list: result.rows[0],
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+        success: false,
+        error: error.message,
+        });
+    }
+});
+
 module.exports = router;
